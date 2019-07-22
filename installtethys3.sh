cfd8b1c536cf2e6815a384782748e4cc67b2195e
rileyhales <rileyhales1@gmail.com>
2019-07-22 17:05:26 -0600

    add aliases

--- installtethys3.sh
+++ installtethys3.sh
@@ -1,11 +1,24 @@
 #!/usr/bin/env bash
+
+# install the tethys repo to a conda environment
 conda create -n tethys -c tethysplatform/label/dev -c tethysplatform -c conda-forge tethysplatform
-cd
+
+# assign aliases to run the tethys environment
 if [[ "$OSTYPE" == "darwin"* ]]; then
-	. .bash_profile
+	BASHPROFILE=".bash_profile"
 elif [[ "$OSTYPE" == "linux-gnu" ]]; then
-	. .bashrc
+	BASHPROFILE=".bashrc"
 fi
+echo "# Tethys Platform" >> ~/${BASHPROFILE}
+echo "alias t='conda activate tethys'" >> ~/${BASHPROFILE}
+echo "alias tms='tethys manage start'" >> ~/${BASHPROFILE}
+
+# initialize tethys settings and databases
 conda activate tethys
 tethys gen settings
 tethys db configure
+conda deactivate tethys
+
+# done
+echo "Finished installing tethys."
+echo "Use 'conda activate tethys' or the 't' alias to enter the tethys environment"
