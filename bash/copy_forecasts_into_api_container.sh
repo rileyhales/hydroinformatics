d078d1ad77b9745284960c85d50efd54022b70c3
Riley Hales <39097632+rileyhales@users.noreply.github.com>
2019-12-05 18:15:30 -0700

    Create copy_forecasts_into_api_container.sh

---
+++ bash/copy_forecasts_into_api_container.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "date --date="yesterday" +%Y/%m/%d"
+
+date=$(date -d '1 day ago' +'%Y%m%d.0')
+host_forecasts_dir=/home/tethys/spt_files/ecmwf
+api_forecasts_dir=/mnt/output/ecmwf
+container_name=gsprestapi_gsp_api_1
+
+
+for region in $(ls $host_forecasts_dir)
+do
+  echo "removing $region"
+  docker exec $container_name rm -rf $api_forecasts_dir/$region
+  echo "creating $region"
+  docker exec $container_name mkdir $api_forecasts_dir/$region/
+  echo "copying $region"
+  docker cp $host_forecasts_dir/$region/$date $container_name:$api_forecasts_dir/$region/
+done
