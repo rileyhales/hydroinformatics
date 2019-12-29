8702223b0bbce6107e6939d2d1499e50763378fe
rileyhales <rileyhales1@gmail.com>
2019-12-28 22:11:36 -0700

    updates to check on era5

--- era5/aggregate.py
+++ era5/aggregate.py
@@ -1,5 +1,7 @@
 import netCDF4
 import os
+import sys
+import datetime
 
 
 # the function that actually subdivides the array
@@ -26,46 +28,72 @@ def subset_list(arr, size):
     return min_arrs, mean_arrs, max_arrs
 
 
-def aggregate(path_Qout, agg_hours=24):
-    # make the file paths
+def aggregate_by_day(path_Qout):
+    agg_hours = 24
+
+    # sort out the file paths
     if not os.path.isfile(path_Qout):
         raise FileNotFoundError('Qout file not found at this path')
+    newfilepath = os.path.join(os.path.dirname(path_Qout), 'Aggregated_Qout.nc4')
 
-    # open the netcdf, create new variables
-    nc = netCDF4.Dataset(filename=path_Qout, mode='r+')
-    nc.createVariable('Qout_min', datatype='f4', dimensions=('time', 'rivid'))
-    nc.createVariable('Qout_mean', datatype='f4', dimensions=('time', 'rivid'))
-    nc.createVariable('Qout_max', datatype='f4', dimensions=('time', 'rivid'))
+    # read the netcdfs
+    source_nc = netCDF4.Dataset(filename=path_Qout, mode='r')
+    new_nc = netCDF4.Dataset(filename=newfilepath, mode='w')
 
-    for i in range(nc.dimensions['rivid'].size):
+    # create rivid and time dimensions
+    new_nc.createDimension('rivid', size=source_nc.dimensions['rivid'].size)
+    new_nc.createDimension('time', size=source_nc.dimensions['time'].size // 24)
+    # create rivid and time variables
+    new_nc.createVariable('rivid', datatype='f4', dimensions=('rivid',), )
+    new_nc.createVariable('time', datatype='f4', dimensions=('time',), )
+    # create the variables for the flows
+    new_nc.createVariable('Qout_min', datatype='f4', dimensions=('time', 'rivid'))
+    new_nc.createVariable('Qout_mean', datatype='f4', dimensions=('time', 'rivid'))
+    new_nc.createVariable('Qout_max', datatype='f4', dimensions=('time', 'rivid'))
+
+    # configure the time variable
+    # start = datetime.datetime(1979, 1, 1, 0, 0)
+    # new_nc.variables['time'][:] = [start + datetime.timedelta(days=i) for i in range(new_nc.dimensions['time'].size)]
+    new_nc.variables['time'][:] = range(new_nc.dimensions['time'].size)
+    new_nc.variables['time'].__dict__['units'] = 'days since 1979-01-01 00:00:00+00:00'
+    new_nc.variables['time'].__dict__['calendar'] = 'gregorian'
+
+    # configure the rivid variable
+    new_nc.variables['rivid'][:] = source_nc.variables['rivid'][:]
+
+    # for each river read the whole time series
+    for i in range(source_nc.dimensions['rivid'].size):
         print(i)
-        tmp = nc.variables['Qout'][:, i]
+        tmp = source_nc.variables['Qout'][:, i]
         min_arrs = []
         mean_arrs = []
         max_arrs = []
-        # if the array is at least 'size' long
+        # if the array is at least 24 steps long
         while len(tmp) >= agg_hours:
-            # take the first 'size' pieces
+            # take the first 24 pieces
             piece = tmp[:agg_hours]
             # get the min, mean, max and put them into a list
             min_arrs.append(min(piece))
             mean_arrs.append(sum(piece) / agg_hours)
             max_arrs.append(max(piece))
-            # drop the first 'size' elements in the array
+            # drop the first 24 steps
             tmp = tmp[agg_hours:]
-            print(tmp.size)
-        # if it doesn't divide perfectly in 'size' len pieces, alert you
+        # if it didn't divide perfectly into 24 hour segments, say something
         if len(tmp) > 0:
             print('Did not divide evenly')
             print(len(tmp))
             print(tmp)
         # write the new arrays to the new variables
-        nc.variables['Qout_min'][:, i] = min_arrs
-        nc.variables['Qout_mean'][:, i] = mean_arrs
-        nc.variables['Qout_max'][:, i] = max_arrs
+        new_nc.variables['Qout_min'][:, i] = min_arrs
+        new_nc.variables['Qout_mean'][:, i] = mean_arrs
+        new_nc.variables['Qout_max'][:, i] = max_arrs
     return
 
 
-path_Qout = '/Users/rileyhales/era5sampledata/sampledata/nam_clearwater/Qout_era5_t640_1hr_19790101to20181231.nc'
+# for running this script from the command line with a script
+if __name__ == '__main__':
+    aggregate_by_day(sys.argv[1])
 
-aggregate(path_Qout, agg_hours=24)
+# oldfilepath = '/Users/rileyhales/Downloads/era5samplefiles/nam_clearwater/Qout_era5_t640_1hr_19790101to20181231.nc'
+# newfilepath = '/Users/rileyhales/Downloads/era5samplefiles/nam_clearwater/Aggregated_Qout.nc4'
+# aggregate_by_day(oldfilepath)
