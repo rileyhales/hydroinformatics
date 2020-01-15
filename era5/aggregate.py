c28021b2a4868823b97640ed11c3bb52ac2bd478
rileyhales <rileyhales1@gmail.com>
2020-01-15 16:23:40 -0700

    solve bug for difference in dimension order

--- era5/aggregate.py
+++ era5/aggregate.py
@@ -6,33 +6,7 @@ import datetime
 import statistics
 
 
-# the function that actually subdivides the array
-def subset_list(arr, size):
-    min_arrs = []
-    mean_arrs = []
-    max_arrs = []
-    # if the array is at least 'size' long
-    while len(arr) >= size:
-        # take the first 'size' pieces
-        piece = arr[:size]
-        # get the min, mean, max and put them into a list
-        min_arrs.append(min(piece))
-        mean_arrs.append(statistics.mean(piece))
-        max_arrs.append(max(piece))
-        # drop the first 'size' elements in the array
-        arr = arr[size:]
-    # if it doesn't divide perfectly in 'size' len pieces, alert you
-    if len(arr) > 0:
-        logging.info('Did not divide evenly')
-        logging.info(len(arr))
-        logging.info(arr)
-    # return the stats about the segmented arrays
-    return min_arrs, mean_arrs, max_arrs
-
-
 def aggregate_by_day(path_Qout):
-    agg_hours = 24
-
     # sort out the file paths
     if not os.path.isfile(path_Qout):
         raise FileNotFoundError('Qout file not found at this path')
@@ -47,8 +21,8 @@ def aggregate_by_day(path_Qout):
     new_nc.createDimension('rivid', size=source_nc.dimensions['rivid'].size)
     new_nc.createDimension('time', size=source_nc.dimensions['time'].size // 24)
     # create rivid and time variables
-    new_nc.createVariable('rivid', datatype='f4', dimensions=('rivid',), )
-    new_nc.createVariable('time', datatype='f4', dimensions=('time',), )
+    new_nc.createVariable('rivid', datatype='f4', dimensions=('rivid',))
+    new_nc.createVariable('time', datatype='f4', dimensions=('time',))
     # create the variables for the flows
     new_nc.createVariable('Qout_min', datatype='f4', dimensions=('time', 'rivid'))
     new_nc.createVariable('Qout_mean', datatype='f4', dimensions=('time', 'rivid'))
@@ -65,9 +39,35 @@ def aggregate_by_day(path_Qout):
 
     # for each river read the whole time series
     num_rivers = source_nc.dimensions['rivid'].size
+    size = 24
     for i in range(num_rivers):
         logging.info(str(i) + '/' + str(num_rivers) + ': Started ' + datetime.datetime.utcnow().strftime("%D at %R"))
-        min_arr, mean_arr, max_arr = subset_list(list(source_nc.variables['Qout'][i, :]), agg_hours)
+
+        min_arr = []
+        mean_arr = []
+        max_arr = []
+
+        # on the sample outputs the dimensions are time, rivid
+        arr = source_nc.variables['Qout'][:, i]
+        # on the rapid docker image, the dimensions are rivid, time, i think
+        # arr = source_nc.variables['Qout'][i, :]
+
+        # if the array is at least 'size' long
+        while len(arr) >= size:
+            # take the first 'size' pieces
+            piece = arr[:size]
+            # get the min, mean, max and put them into a list
+            min_arr.append(min(piece))
+            mean_arr.append(statistics.mean(piece))
+            max_arr.append(max(piece))
+            # drop the first 'size' elements in the array
+            arr = arr[size:]
+        # if it doesn't divide perfectly in 'size' len pieces, alert you
+        if len(arr) > 0:
+            logging.info('Did not divide evenly')
+            logging.info(len(arr))
+            logging.info(arr)
+
         # write the new arrays to the new variables
         logging.info('  writing Qmin variables')
         new_nc.variables['Qout_min'][:, i] = min_arr
@@ -90,8 +90,10 @@ if __name__ == '__main__':
     """
     # enable logging to track the progress of the workflow and for debugging
     logging.basicConfig(filename=sys.argv[2], filemode='w', level=logging.INFO, format='%(message)s')
+    # logging.basicConfig(filename='/Users/rileyhales/Downloads/era5samplefiles/log.log', filemode='w', level=logging.INFO, format='%(message)s')
     logging.info('ERA5 aggregation started on ' + datetime.datetime.utcnow().strftime("%D at %R"))
     aggregate_by_day(sys.argv[1])
+    # aggregate_by_day('/Users/rileyhales/Downloads/era5samplefiles/nam_clearwater/Qout_era5_t640_1hr_19790101to20181231.nc')
     logging.info('')
     logging.info('FINISHED')
     logging.info(datetime.datetime.utcnow().strftime("%D at %R"))
