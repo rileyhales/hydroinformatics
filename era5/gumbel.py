1e81ef663d3e862f8203bb7383db546569fc2755
rileyhales <rileyhales1@gmail.com>
2020-02-05 11:32:55 -0700

    fix methods for running script. different print statements

--- era5/gumbel.py
+++ era5/gumbel.py
@@ -27,21 +27,24 @@ def daily_to_yearly_max_flow(daily_flow_list, start_yr, end_yr):
     return yearly_max_flows
 
 
-def gumbel_return_periods(path_Qout, forcing):
-    if forcing == 'interim':
-        start_yr = 1980
-        end_yr = 2015
-        flow_var = 'Qout'
-    elif forcing == 'era5':
-        start_yr = 1979
-        end_yr = 2018
-        flow_var = 'Qout_max'
-    else:
-        raise ValueError('choose interim or era5')
-
+def gumbel_return_periods(path_Qout):
     # sort out the file paths
     if not os.path.isfile(path_Qout):
         raise FileNotFoundError('Qout file not found at this path')
+
+    if 'erai' in os.path.basename(path_Qout):
+        start_yr = 1980
+        end_yr = 2014
+        # series_len = 12784
+        flow_var = 'Qout'
+    elif 'era5' in os.path.basename(path_Qout):
+        start_yr = 1979
+        end_yr = 2018
+        # series_len = 350616
+        flow_var = 'Qout_max'
+    else:
+        raise ValueError('unrecognized file, should be erai or era5')
+
     rp_nc_path = os.path.join(os.path.dirname(path_Qout), 'Gumbel_return_periods.nc4')
 
     # read the netcdfs
@@ -68,8 +71,9 @@ def gumbel_return_periods(path_Qout, forcing):
     # for each river read the whole time series
     num_rivers = source_nc.dimensions['rivid'].size
     for i in range(num_rivers):
-        logging.info(str(i) + '/' + str(num_rivers) + ': Started ' + datetime.datetime.utcnow().strftime("%D at %R"))
-        yearly_max_flows = daily_to_yearly_max_flow(source_nc.variables[flow_var][:, i], start_yr, end_yr)
+        logging.info(str(i) + '/' + str(num_rivers))
+        # order of slice is i, : / if it fails, it came from an old version of rapid and order is :, i
+        yearly_max_flows = daily_to_yearly_max_flow(source_nc.variables[flow_var][i, :], start_yr, end_yr)
         xbar = statistics.mean(yearly_max_flows)
         std = statistics.stdev(yearly_max_flows, xbar=xbar)
         logging.info('xbar: ' + str(xbar))
@@ -91,14 +95,35 @@ def gumbel_return_periods(path_Qout, forcing):
 
 
 # for running this script from the command line with a script
-if __name__ == '__main__':
-    """
-    sys.argv[0] this script e.g. gumbel.py
-    sys.argv[1] path to Qout file
-    sys.argv[2] interim or era5
-    sys.argv[3] path to log file
-    """
-    # enable logging to track the progress of the workflow and for debugging
-    logging.basicConfig(filename=sys.argv[3], filemode='w', level=logging.INFO, format='%(message)s')
-    logging.info('Gumbel Return Period Processing started on ' + datetime.datetime.utcnow().strftime("%D at %R"))
-    gumbel_return_periods(sys.argv[1], sys.argv[2])
+# if __name__ == '__main__':
+#     """
+#     sys.argv[0] this script e.g. gumbel.py
+#     sys.argv[1] path to Qout file
+#     sys.argv[2] path to log file
+#     """
+#     # enable logging to track the progress of the workflow and for debugging
+#     logging.basicConfig(filename=sys.argv[2], filemode='w', level=logging.info, format='%(message)s')
+#     logging.info('Gumbel Return Period Processing started on ' + datetime.datetime.utcnow().strftime("%D at %R"))
+#     gumbel_return_periods(sys.argv[1])
+
+
+# logging.basicConfig(filename='/Users/rileyhales/Downloads/INTERIM/log.log', filemode='w', level=logging.INFO, format='%(message)s')
+# intfiles = (
+#     '/Users/rileyhales/Downloads/INTERIM/africa-geoglows/Qout_erai_t511_24hr_19800101to20141231.nc',
+#     '/Users/rileyhales/Downloads/INTERIM/australia-geoglows/Qout_erai_t511_24hr_19800101to20141231.nc',
+#     '/Users/rileyhales/Downloads/INTERIM/central_america-geoglows/Qout_erai_t511_24hr_19800101to20141231.nc',
+#     '/Users/rileyhales/Downloads/INTERIM/central_asia-geoglows/Qout_erai_t511_24hr_19800101to20141231.nc',
+#     '/Users/rileyhales/Downloads/INTERIM/east_asia-geoglows/Qout_erai_t511_24hr_19800101to20141231.nc',
+#     '/Users/rileyhales/Downloads/INTERIM/europe-geoglows/Qout_erai_t511_24hr_19800101to20141231.nc',
+#     '/Users/rileyhales/Downloads/INTERIM/japan-geoglows/Qout_erai_t511_24hr_19800101to20141231.nc',
+#     '/Users/rileyhales/Downloads/INTERIM/middle_east-geoglows/Qout_erai_t511_24hr_19800101to20141231.nc',
+#     '/Users/rileyhales/Downloads/INTERIM/north_america-geoglows/Qout_erai_t511_24hr_19800101to20141231.nc',
+#     '/Users/rileyhales/Downloads/INTERIM/south_america-geoglows/Qout_erai_t511_24hr_19800101to20141231.nc',
+#     '/Users/rileyhales/Downloads/INTERIM/south_asia-geoglows/Qout_erai_t511_24hr_19800101to20141231.nc',
+#     '/Users/rileyhales/Downloads/INTERIM/west_asia-geoglows/Qout_erai_t511_24hr_19800101to20141231.nc',
+# )
+#
+# for intfile in intfiles:
+#     print(os.path.basename(os.path.dirname(intfile)))
+#     logging.info('Gumbel Return Period Processing started on ' + datetime.datetime.utcnow().strftime("%D at %R"))
+#     gumbel_return_periods(intfile)
