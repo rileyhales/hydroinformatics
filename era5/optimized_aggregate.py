3e500234006ff88336a44b78f51d94b49d319df1
rileyhales <rileyhales1@gmail.com>
2020-04-24 11:59:10 -0600

    manually change the slice hours

--- era5/optimized_aggregate.py
+++ era5/optimized_aggregate.py
@@ -53,7 +53,7 @@ def aggregate_by_day(path_Qout, write_frequency=500):
         exact = True
     else:
         raise RuntimeError('unexpected length of times found.')
-    number_days = number_hours / 24
+    number_days = number_hours // 24
     logging.info('number of rivers: ' + str(num_rivers))
 
     # create a set of indices for slicing the array in larger groups
@@ -92,6 +92,7 @@ def aggregate_by_day(path_Qout, write_frequency=500):
         # max_arr = []
 
         # if the array is at least 'size' long
+        logging.info('splitting the array into {0) days'.format(number_days))
         for day_flows in np.split(arr, number_days):
             # min_arr.append(day_flows.min(axis=0))
             mean_arr.append(day_flows.mean(axis=0))
