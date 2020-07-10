f851b1333aeb33f2b3b69139df9718ced223a45a
rileyhales <rileyhales1@gmail.com>
2020-07-09 19:16:52 -0600

    more filtering options, plotting as function

--- bias_adj_propagation/dev_monthly_test.py
+++ bias_adj_propagation/dev_monthly_test.py
@@ -73,7 +73,8 @@ def get_scalar_bias_fdc(first_series, seconds_series):
     first_fdc = compute_flow_duration_curve(first_series)
     second_fdc = compute_flow_duration_curve(seconds_series)
     ratios = np.divide(first_fdc['Flow'].values.flatten(), second_fdc['Flow'].values.flatten())
-    scalars_df = pd.DataFrame(np.transpose([first_fdc.values[:, 0], ratios]))
+    columns = (first_fdc.columns[0], 'Scalars')
+    scalars_df = pd.DataFrame(np.transpose([first_fdc.values[:, 0], ratios]), columns=columns)
     scalars_df.replace(np.inf, np.nan, inplace=True)
     scalars_df.dropna(inplace=True)
 
@@ -81,7 +82,9 @@ def get_scalar_bias_fdc(first_series, seconds_series):
 
 
 def propagate_correction(sim_data: pd.DataFrame, obs_data: pd.DataFrame, sim_data_to_correct,
-                         drop_outliers: bool = False, outlier_threshold: int or float = 2.5) -> pd.DataFrame:
+                         drop_outliers: bool = False, outlier_threshold: int or float = 2.5,
+                         filter_scalar_fdc: bool = True, fdc_range: tuple = (10, 90),
+                         extrapolate_method: str = 'average') -> pd.DataFrame:
     # list of the unique months in the historical simulation. should always be 1->12 but just in case...
     unique_simulation_months = sorted(set(sim_data.index.strftime('%m')))
     dates = []
@@ -109,9 +112,21 @@ def propagate_correction(sim_data: pd.DataFrame, obs_data: pd.DataFrame, sim_dat
 
         scalar_fdc = get_scalar_bias_fdc(mon_obs_fdc['Flow'].values.flatten(), mon_sim_fdc['Flow'].values.flatten())
 
+        if filter_scalar_fdc:
+            scalar_fdc = scalar_fdc[scalar_fdc['Exceedence Probability'] >= fdc_range[0]]
+            scalar_fdc = scalar_fdc[scalar_fdc['Exceedence Probability'] <= fdc_range[1]]
+
         # create the interpolator for the month
-        to_scalar = interpolate.interp1d(scalar_fdc.values[:, 0], scalar_fdc.values[:, 1],
-                                         fill_value='extrapolate', kind='nearest')
+        if extrapolate_method == 'linear':
+            to_scalar = interpolate.interp1d(scalar_fdc.values[:, 0], scalar_fdc.values[:, 1], fill_value='extrapolate')
+        elif extrapolate_method == 'nearest':
+            to_scalar = interpolate.interp1d(scalar_fdc.values[:, 0], scalar_fdc.values[:, 1],
+                                             fill_value='extrapolate', kind='nearest')
+        elif extrapolate_method == 'average':
+            to_scalar = interpolate.interp1d(scalar_fdc.values[:, 0], scalar_fdc.values[:, 1],
+                                             fill_value=np.mean(scalar_fdc.values[:, 1]), bounds_error=False)
+        else:
+            raise ValueError('Invalid extrapolation method provided')
 
         # determine the percentile of each uncorrected flow using the monthly fdc
         x = monthly_cor.values.flatten()
@@ -132,8 +147,45 @@ def propagate_correction(sim_data: pd.DataFrame, obs_data: pd.DataFrame, sim_dat
     return corrected
 
 
-# collect_data(9017261, 32037030, 9015333, 32097010)
-collect_data(9012999, 22057070, 9012650, 22057010)
+def plot_results(sim, obs, bc, bcp, title):
+    sim_dates = sim.index.tolist()
+    scatters = [
+        go.Scatter(
+            name='Propagated Corrected (Experimental)',
+            x=bcp.index.tolist(),
+            y=bcp['Propagated Corrected Streamflow'].values.flatten(),
+        ),
+        go.Scatter(
+            name='Bias Corrected (Jorges Method)',
+            x=sim_dates,
+            y=bc.values.flatten(),
+        ),
+        go.Scatter(
+            name='Simulated (ERA 5)',
+            x=sim_dates,
+            y=sim.values.flatten(),
+        ),
+        go.Scatter(
+            name='Observed',
+            x=obs.index.tolist(),
+            y=obs.values.flatten(),
+        ),
+        go.Scatter(
+            name='Percentile',
+            x=sim_dates,
+            y=bcp['MonthlyPercentile'].values.flatten(),
+        ),
+        go.Scatter(
+            name='Scalar',
+            x=sim_dates,
+            y=bcp['Scalars'].values.flatten(),
+        ),
+    ]
+    go.Figure(scatters, layout={'title': title}).show()
+    return
+
+collect_data(9017261, 32037030, 9015333, 32097010)
+# collect_data(9012999, 22057070, 9012650, 22057010)
 
 # Read all as csv
 start_flow = pd.read_csv('start_flow.csv', index_col=0)
@@ -148,39 +200,29 @@ downstream_flow.index = pd.to_datetime(downstream_flow.index)
 downstream_ideam_flow.index = pd.to_datetime(downstream_ideam_flow.index)
 downstream_bc_flow.index = pd.to_datetime(downstream_bc_flow.index)
 
-downstream_prop_correct = propagate_correction(start_flow, start_ideam_flow, downstream_flow, drop_outliers=True, outlier_threshold=.5)
+# downstream_prop_correct = propagate_correction(start_flow, start_ideam_flow, downstream_flow, filter_scalar_fdc=True, extrapolate_method='average')
 
-sim_dates = downstream_flow.index.tolist()
-scatters = [
-    go.Scatter(
-        name='Propagated Corrected (Experimental)',
-        x=downstream_prop_correct.index.tolist(),
-        y=downstream_prop_correct['Propagated Corrected Streamflow'].values.flatten(),
-    ),
-    go.Scatter(
-        name='Bias Corrected (Jorges Method)',
-        x=sim_dates,
-        y=downstream_bc_flow.values.flatten(),
-    ),
-    go.Scatter(
-        name='Simulated (ERA 5)',
-        x=sim_dates,
-        y=downstream_flow.values.flatten(),
-    ),
-    go.Scatter(
-        name='Observed',
-        x=downstream_ideam_flow.index.tolist(),
-        y=downstream_ideam_flow.values.flatten(),
-    ),
-    go.Scatter(
-        name='Percentile',
-        x=sim_dates,
-        y=downstream_prop_correct['MonthlyPercentile'].values.flatten(),
-    ),
-    go.Scatter(
-        name='Scalar',
-        x=sim_dates,
-        y=downstream_prop_correct['Scalars'].values.flatten(),
-    ),
-]
-go.Figure(scatters).show()
+# plot_results(downstream_flow, downstream_ideam_flow, downstream_bc_flow, downstream_prop_correct, 'Full Series')
+downstream_prop_correct = propagate_correction(start_flow, start_ideam_flow, downstream_flow)
+plot_results(downstream_flow, downstream_ideam_flow, downstream_bc_flow, downstream_prop_correct, 'Correct Monthly')
+
+# downstream_prop_correct = propagate_correction(start_flow, start_ideam_flow, downstream_flow, drop_outliers=True, outlier_threshold=1)
+# plot_results(downstream_flow, downstream_ideam_flow, downstream_bc_flow, downstream_prop_correct, 'Correct Monthly - Drop input outliers @ 1z, linear extrapolation')
+# downstream_prop_correct = propagate_correction(start_flow, start_ideam_flow, downstream_flow, drop_outliers=True, outlier_threshold=1, extrapolate_method='nearest')
+# plot_results(downstream_flow, downstream_ideam_flow, downstream_bc_flow, downstream_prop_correct, 'Correct Monthly - Drop input outliers @ 1z, nearest extrapolation')
+# downstream_prop_correct = propagate_correction(start_flow, start_ideam_flow, downstream_flow, drop_outliers=True, outlier_threshold=1, extrapolate_method='average')
+# plot_results(downstream_flow, downstream_ideam_flow, downstream_bc_flow, downstream_prop_correct, 'Correct Monthly - Drop input outliers @ 1z, average extrapolation')
+
+downstream_prop_correct = propagate_correction(start_flow, start_ideam_flow, downstream_flow, filter_scalar_fdc=True)
+plot_results(downstream_flow, downstream_ideam_flow, downstream_bc_flow, downstream_prop_correct, 'Correct Monthly - Using the middle of the scalar fdc (10-90%), linear extrapolation')
+downstream_prop_correct = propagate_correction(start_flow, start_ideam_flow, downstream_flow, filter_scalar_fdc=True, extrapolate_method='nearest')
+plot_results(downstream_flow, downstream_ideam_flow, downstream_bc_flow, downstream_prop_correct, 'Correct Monthly - Using the middle of the scalar fdc (10-90%), nearest extrapolation')
+downstream_prop_correct = propagate_correction(start_flow, start_ideam_flow, downstream_flow, filter_scalar_fdc=True, extrapolate_method='average')
+plot_results(downstream_flow, downstream_ideam_flow, downstream_bc_flow, downstream_prop_correct, 'Correct Monthly - Using the middle of the scalar fdc (10-90%), average extrapolation')
+
+downstream_prop_correct = propagate_correction(start_flow, start_ideam_flow, downstream_flow, filter_scalar_fdc=True, fdc_range=(10, 80))
+plot_results(downstream_flow, downstream_ideam_flow, downstream_bc_flow, downstream_prop_correct, 'Correct Monthly - Using the middle of the scalar fdc (10-80%), linear extrapolation')
+downstream_prop_correct = propagate_correction(start_flow, start_ideam_flow, downstream_flow, filter_scalar_fdc=True, fdc_range=(10, 80), extrapolate_method='nearest')
+plot_results(downstream_flow, downstream_ideam_flow, downstream_bc_flow, downstream_prop_correct, 'Correct Monthly - Using the middle of the scalar fdc (10-80%), nearest extrapolation')
+downstream_prop_correct = propagate_correction(start_flow, start_ideam_flow, downstream_flow, filter_scalar_fdc=True, fdc_range=(10, 80), extrapolate_method='average')
+plot_results(downstream_flow, downstream_ideam_flow, downstream_bc_flow, downstream_prop_correct, 'Correct Monthly - Using the middle of the scalar fdc (10-80%), average extrapolation')
